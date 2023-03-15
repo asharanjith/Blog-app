2b68a8a17f41b5a8fa2bbab1b0fa4a91c8c975c3
asharanjith <ashasvijayan@gmail.com>
2023-03-15 23:45:27 +0400

    Ad view files for posts and users

--- db/migrate/20230307143603_create_posts.rb
+++ db/migrate/20230307143603_create_posts.rb
@@ -3,8 +3,8 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.integer :commentsCounter
-      t.integer :likesCounter
+      t.integer :commentsCounter, default: 0
+      t.integer :likesCounter, default: 0
 
       t.timestamps
     end
