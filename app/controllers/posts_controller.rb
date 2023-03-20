b8df65877e3594677ccd9b234d0984b2ad1f74f2
unknown <Riyanagwl@gmail.com>
2023-03-20 12:26:24 +0300

    fix linters error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -12,14 +13,18 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
   def index
     @user = User.find_by(id: params[:user_id])
     @posts = @user.posts.includes(:comments)
   end
+
   def show
     @post = Post.find_by(author_id: params[:user_id], id: params[:id]) || 'No posts yet'
   end
+
   private
+
   def post_params
     params.require(:post).permit(:title, :text).merge(author_id: current_user.id)
   end
