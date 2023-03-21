443cdd861eba4d626eef93343651fc8fb2243dd6
asharanjith <ashasvijayan@gmail.com>
2023-03-21 20:32:44 +0400

    Add styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
+  def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :photo, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :bio, :photo, :email, :password, :current_password)
