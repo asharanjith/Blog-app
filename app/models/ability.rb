e179a046059adb164b8b658230021876fe0f864f
asharanjith <ashasvijayan@gmail.com>
2023-03-22 16:34:57 +0400

    Remove comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -33,23 +33,3 @@ class Ability
     # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
   end
 end
-
-# class Ability
-#   include CanCan::Ability
-
-#   def initialize(user)
-#     if user.role == 'admin'
-#       can :manage, :all
-#     else
-#       can :destroy, Post do |post|
-#         post.author == user
-#       end
-#       can :destroy, Comment do |comment|
-#         comment.author == user
-#       end
-#       can :create, Post
-#       can :create, Comment
-#       can :read, :all
-#     end
-#   end
-# end
